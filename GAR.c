0b0449acdd46288785329b5339ba26873c73eb0a
Eryk Bialczak <erykbialczak@gmail.com>
2026-03-29 21:00:47 +0100

    Dodaj pliki projektów.

---
+++ GAR.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct //struct for containing crucial data
+{
+	char game[40];
+	char email[30];
+	char password[30];
+}AccountsT;
+
+void main()
+{
+
+}
