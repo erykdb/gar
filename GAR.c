3e93d6ab63ef8cfc3622a0eb955524eddc0386c0
Eryk Bialczak <erykbialczak@gmail.com>
2026-03-30 14:24:42 +0100

    Added a while loop to control the code and 2 functions to add and view your data

--- GAR.c
+++ GAR.c
@@ -11,6 +11,9 @@ typedef struct
 	char password[30];
 }AccountsT;
 
+void viewPasses(AccountsT* myArray, int arraySize);
+void addPasses(AccountsT* myArray, int arraySize);
+
 void main()
 {
 	AccountsT* accPass; 
@@ -28,4 +31,34 @@ void main()
 		printf("The memory could not be allocated\n");
 		return;
 	}
+
+	printf("Please 1 to view email and password for a game\n");
+	printf("Please 2 to add email and passowrd for a game\n");
+	printf("Please -1 to exit\n");
+	scanf("%d", &choice);
+
+	while (choice != -1)
+	{
+		if (choice == 1)
+			viewPasses(accPass, passAmount);
+		else if (choice == 2)
+			addPasses(accPass, passAmount);
+
+		printf("Please 1 to search for a book\n");
+		printf("Please 2 to add for a book\n");
+		printf("Please -1 to exit\n");
+		scanf("%d", &choice);
+	}
+
+}
+
+void viewPasses(AccountsT* myArray, int arraySize)
+{
+
+}
+
+void addPasses(AccountsT* myArray, int arraySize)
+{
+
+}
 
