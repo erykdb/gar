20c1dac19941f13554c2e4db5666b64db0d54728
Eryk Bialczak <erykbialczak@gmail.com>
2026-03-30 19:24:23 +0100

    added a display from file function, doing the final test to make sure the code runs properly and adding final comments

--- GAR.c
+++ GAR.c
@@ -12,18 +12,19 @@ typedef struct//struct for data that will be saved in a file
 	char email[30];
 	char password[30];
 }AccountsT;
+
 //introducing all of the functions so that they can work afterwards since C reads code from top to bottom
 void viewPasses(AccountsT* myArray, int arraySize);
 void addPasses(AccountsT* myArray, int arraySize);
 void saveDataToFile(AccountsT* myArray, int arraySize);
 void appendDataToFile(AccountsT* myArray, int arraySize);
 void deleteDataFromFile(AccountsT* myArray, int arraySize);
+void readDataFromFile(AccountsT* myArray, int arraySize);
 
 void main() 
 {
 	//introducing variables for main function
 	AccountsT* accPass;
-	FILE* fp;
 	int i;
 	int passAmount;
 	int choice;
@@ -47,10 +48,11 @@ void main()
 
 	//this is the menu of the app, here you have printf's and a while loop, to add, show, save or delete data as you wish
 	printf("\nPlease write 1 to view email and password for a game\n");
-	printf("Please write 2 to add email and passowrd for a game\n");
+	printf("Please write 2 to add email and password for a game\n");
 	printf("Please write 3 to save your local data to a file\n");
 	printf("Please write 4 to append your local data to a file\n");
 	printf("Please write 5 to delete currently stored data in a file\n");
+	printf("Please write 6 to display currently stored data from a file\n");
 	printf("Please write -1 to exit\n");
 	scanf("%d", &choice);
 
@@ -66,12 +68,15 @@ void main()
 			appendDataToFile(accPass, passAmount);
 		else if (choice == 5)
 			deleteDataFromFile(accPass, passAmount);
+		else if (choice == 6)
+			readDataFromFile(accPass, passAmount);
 
 		printf("\nPlease write 1 to view email and password for a game\n");
-		printf("Please write 2 to add email and passowrd for a game\n");
+		printf("Please write 2 to add email and password for a game\n");
 		printf("Please write 3 to save your local data to a file\n");
 		printf("Please write 4 to append your local data to a file\n");
 		printf("Please write 5 to delete currently stored data in a file\n");
+		printf("Please write 6 to display currently stored data from a file\n");
 		printf("Please write -1 to exit\n");
 		scanf("%d", &choice);
 	}
@@ -89,7 +94,7 @@ void viewPasses(AccountsT* myArray, int arraySize)
 	int gameFound = 0;
 
 	printf("\nPlease enter the game you are looking for(use _ as spaces between the words)\n");
-	scanf("%s", &searchGame);
+	scanf("%s", searchGame);
 
 	for (i = 0; i < arraySize; i++)
 	{
@@ -111,7 +116,7 @@ void viewPasses(AccountsT* myArray, int arraySize)
 	if (!gameFound)
 	{
 		printf("Game not found.\n");
-		return;
+		
 	}
 }
 
@@ -140,7 +145,7 @@ void addPasses(AccountsT* myArray, int arraySize)
 			return;
 		}
 	}
-	printf("Your Details have been allocated!\n");
+	printf("No more empty slots for games available!\n");
 }
 
 /*
@@ -154,6 +159,11 @@ void saveDataToFile(AccountsT* myArray, int arraySize)
 	int i;
 
 	fp = fopen("accounts.txt", "w");
+	if (fp == NULL)
+	{
+		printf("Error while opening the file!\n");
+		return;
+	}
 
 	for (i = 0; i < arraySize; i++)
 	{
@@ -179,6 +189,11 @@ void appendDataToFile(AccountsT* myArray, int arraySize)
 	int i;
 
 	fp = fopen("accounts.txt", "a");
+	if (fp == NULL)
+	{
+		printf("Error while opening the file!\n");
+		return;
+	}
 
 	for (i = 0; i < arraySize; i++)
 	{
@@ -202,9 +217,48 @@ void deleteDataFromFile(AccountsT* myArray, int arraySize)
 	FILE* fp;
 
 	fp = fopen("accounts.txt", "w");
+	if (fp == NULL)
+	{
+		printf("Error while opening the file!\n");
+		return;
+	}
 
 	fclose(fp);
 
 	printf("Data successfully deleted from accounts.txt!\n");
+}
+
+/*
+* This function as the name implies is reading currently stored data from 
+* accounts.txt to the code, so that the user can determine whether everything is as they want it.
+*/
+void readDataFromFile(AccountsT* myArray, int arraySize)
+{
+	FILE* fp;
+	int i = 0;
+	int inputsNum;
+
+	fp = fopen("accounts.txt", "r");
+	if (fp == NULL)
+	{
+		printf("Error while opening the file!\n");
+		return;
+	}
+
+	printf("\n"); //added so it looks a bit clearer
+
+	while (!feof(fp))
+	{
+		inputsNum = fscanf(fp, "%s %s %s", (myArray + i)->game, (myArray + i)->email, (myArray + i)->password);
+
+		if (inputsNum == 3)
+		{
+			printf("%s %s %s\n", (myArray + i)->game, (myArray + i)->email, (myArray + i)->password);
+		}
+	}
+
+	fclose(fp);
+
+	printf("Data successfully loaded from accounts.txt!\n");
 }
 
